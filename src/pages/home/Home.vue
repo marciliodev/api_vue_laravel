a780edacba534814e8768e64f38063673292299f
Marcílio Pereira Aguiar <marcilio.pa@hotmail.com>
2018-08-22 12:00:01 -0300

    Aula 21 Completa - Melhoramentos de Rotas

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,13 +2,27 @@
 
   <site-template>
 
-    <publicar-conteudo-vue />
-   
-    <card-conteudo-vue 
-        perfil="https://materializecss.com/images/yuna.jpg"
-        nome="Maria Silva" 
-        data="13/01/2018 13:30">
+    <span slot="menuesquerdo">
+      <div class="row valign-wrapper">
+        <grid-vue tamanho="4">
+          <img src="https://materializecss.com/images/yuna.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+        </grid-vue>
+        <grid-vue tamanho="8">
+          <span class="black-text">
+            <h5>Maria Silva</h5>
+            Add the "circle" class to it to make it appear circular.
+          </span>
+        </grid-vue>
+      </div>
+    </span>
 
+    <span slot="principal">
+      <publicar-conteudo-vue />
+   
+      <card-conteudo-vue 
+          perfil="https://materializecss.com/images/yuna.jpg"
+          nome="Maria Silva" 
+          data="13/01/2018 13:30">
 
       <card-detalhe-vue 
         img="https://materializecss.com/images/sample-1.jpg" 
@@ -16,6 +30,7 @@
         texto="I am a very simple card. I am good at containing small bits of information.
           I am convenient because I require little markup to use effectively."/>   
       </card-conteudo-vue>
+    </span>
 
   </site-template>
  
